a5f161729dadd61ca0df4bd553febe5d498e8af2
Fatmir Boja <fatmirboja@hotmail.com>
2018-09-10 21:25:12 +0200

    Encoder Decoder Model

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class EncoderCNN(nn.Module):
         resnet = models.resnet50(pretrained=True)
         for param in resnet.parameters():
             param.requires_grad_(False)
-        
+
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.embed = nn.Linear(resnet.fc.in_features, embed_size)
@@ -19,14 +19,35 @@ class EncoderCNN(nn.Module):
         features = features.view(features.size(0), -1)
         features = self.embed(features)
         return features
-    
+
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
     def forward(self, features, captions):
-        pass
+        features = features.view(len(features), 1, -1)
+        print(features)
+
+        embeddings = self.embed(captions[:, :-1])
+        print(embeddings)
+
+        inputs = torch.cat((features, embeddings), 1)
+        print(input)
+
+        out, hidden = self.lstm(inputs)
+
+        out = self.fc(out)
+
+        return out
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
