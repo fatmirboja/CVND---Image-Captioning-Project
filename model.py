9924f545593cd4832b21874047a9195c577f93a2
Fatmir Boja <fatmirboja@hotmail.com>
2018-09-12 20:48:04 +0200

    inference and cleaning of the result

--- model.py
+++ model.py
@@ -30,33 +30,51 @@ class DecoderRNN(nn.Module):
         self.num_layers = num_layers
 
         self.embed = nn.Embedding(vocab_size, embed_size)
+        # Important: batch_first=True, since we 
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, features, captions):
+        # reshape the features
         features = features.view(len(features), 1, -1)
 
+        # embed the captions
         embeddings = self.embed(captions[:, :-1])
 
+        # concatenate the image features with the embeddings
         inputs = torch.cat((features, embeddings), 1)
+        
+        # pass through the lstm
         out, hidden = self.lstm(inputs)
-
+        
+        # fully connnected layer
         out = self.fc(out)
 
         return out
 
-    def sample(self, inputs, states=None, max_len=20):
+    def sample(self, features, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        sample = []
         
-        embeddings = None
+        # In the first step the input consists only of the image features.
+        # Later the input is "extended" by the words that the LSTM predicts.
+        inputs = features
         
         for i in range(max_len):
+            
+            # pass through the lstm
             out, hidden = self.lstm(inputs)
+            
+            # pass through the fully connected layer
             out = self.fc(out)
-            out = out.squeeze(1).max(1)[1]
-            sample.append(out.item())
-            inputs = self.embed(out)
-            inputs = inputs.unsqueeze(1)
+            
+            # get the captions with the highest value
+            captions = out.argmax(2)
+            
+            # now prepare the input for the next iteration
+            # embed the captions
+            embeddings = self.embed(captions)
+            
+            # input for the next iteration: concatenate the image features with the embeddings
+            inputs = torch.cat((features, embeddings), 1)
           
-        return sample
+        return captions.tolist()[0]
